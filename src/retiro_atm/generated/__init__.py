cb1b375fda65567e0ec7a619ed9e4c6c9e5e3509
Angelo <angelomejia970@gmail.com>
2026-03-20 10:19:56 -0500

    feat(fraud-module): Aislar modulo de fraude - eliminar fuga, morosidad y retiro_atm del indice

--- src/retiro_atm/generated/__init__.py
+++
@@ -1,2 +0,0 @@
-# src/retiro_atm/generated/__init__.py
-from retiro_atm.generated.synthetic_data_service import ejecutar_sync
--- src/retiro_atm/monitoring/repository/__init__.py
+++
@@ -1,2 +0,0 @@
-# src/retiro_atm/monitoring/repository/__init__.py
-from  retiro_atm.monitoring.repository.db_queries import *
